343c7b82dec2c64153ca9b15ae9dd88f85074df2
haappyy <57199984+haappyy@users.noreply.github.com>
2021-10-31 06:49:03 +0100

    Add a test for the validation error message

--- src/test/java/com/example/ExampleControllerTest.java
+++ src/test/java/com/example/ExampleControllerTest.java
@@ -4,8 +4,10 @@ import io.micronaut.http.HttpRequest;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +24,15 @@ public class ExampleControllerTest {
         HttpResponse response = httpClient.toBlocking().exchange(HttpRequest.POST("/validate","{\"testvalue\":\"test\", \"value\": 200}"));
         assertEquals(HttpResponse.ok().code(),
                 response.code());
+    }
+
+    @Test()
+    void testErrorMessage() {
+        HttpClientResponseException exception = Assertions.assertThrows(HttpClientResponseException.class, () -> {
+            httpClient.toBlocking().retrieve(HttpRequest.POST("/validate","...."));
+        });
+        assertEquals(exception.getResponse().body(), "A JSONObject text must begin with '{' at 1 [character 2 line 1]\n" +
+                "A JSONArray text must start with '[' at 1 [character 2 line 1]");
     }
 
 }
