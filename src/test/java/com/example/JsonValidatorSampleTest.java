ee200c19897c9a6bfa060346e7b03254be35f63c
haappyy <57199984+haappyy@users.noreply.github.com>
2021-10-31 06:13:04 +0100

    Add tests for the new Validator error messages

--- src/test/java/com/example/JsonValidatorSampleTest.java
+++ src/test/java/com/example/JsonValidatorSampleTest.java
@@ -3,12 +3,10 @@ package com.example;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @MicronautTest
 class JsonValidatorSampleTest {
@@ -26,16 +24,26 @@ class JsonValidatorSampleTest {
 
     @Test
     void validateJsonSimpleString(){
-        assertTrue(jsonValidator.isValidJson("{}"));
+        assertEquals(JsonValidator.ValidationResult.VALID,jsonValidator.isValidJson("{}").getValidationResult());
     }
 
     @Test
     void validateJsonMultipleValues(){
-        assertTrue(jsonValidator.isValidJson("{\"testvalue\":\"test\", \"value\": 200}"));
+        assertEquals(JsonValidator.ValidationResult.VALID,jsonValidator.isValidJson("{\"testvalue\":\"test\", \"value\": 200}").getValidationResult());
     }
 
     @Test
     void validateJsonInvalid(){
-        assertFalse(jsonValidator.isValidJson("}[..,"));
+        assertEquals(JsonValidator.ValidationResult.INVALID,jsonValidator.isValidJson("}[..,").getValidationResult());
+    }
+
+    @Test
+    void validateJsonInvalidErrorMessage(){
+        assertEquals("A JSONObject text must begin with '{' at 1 [character 2 line 1]",jsonValidator.isValidJson("}[..,").getErrorMessageObject().get());
+    }
+
+    @Test
+    void validateJsonInvalidErrorMessageArray(){
+        assertEquals("Expected a ',' or ']' at 1 [character 2 line 1]",jsonValidator.isValidJson("[").getErrorMessageArray().get());
     }
 }
